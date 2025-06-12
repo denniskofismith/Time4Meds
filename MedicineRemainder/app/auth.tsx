9cd710564cf24637ea4120692094e2f1e5d1ae3e
denniskofismith <denniskofismith@gmail.com>
2025-06-12 18:15:04 +0000

    Splash Screen

---
+++ MedicineRemainder/app/auth.tsx
@@ -0,0 +1,4 @@
+import {useState,useEffect} from 'react'
+import {View,Text, StyleSheet, TouchableOpacity, Dimensions}from 'react-native'
+import {useRouter} from 'expo-router'
+import * as LocalAuthentication from 'expo-local-authentication'
